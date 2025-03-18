4bf41eb3a3262a96c0dc9dd0dba38b86d7d54c5d
Gabriel Rodrigues <gdev.ti56@gmail.com>
2025-03-18 17:19:48 -0300

    primeiro commit python

---
+++ python/funcao.py
@@ -0,0 +1,3 @@
+def Olamundo(): #def é  palavra reservada para definir uma função, que tem um nome e dentro dos parenteses pode conter ou não parâmetros
+    print("ola mundo") #conteudo da função
+Olamundo() #chamada da função
