7ef8e06fd3856f048be934dc359530a01c8d11d0
Gabriel Rodrigues <gdev.ti56@gmail.com>
2025-03-20 15:59:02 -0300

    estruturas de condição

---
+++ java/Condicoes.java
@@ -0,0 +1,10 @@
+package java;
+
+public class Condicoes {
+    int idade = 18;
+    if(idade >=18){
+        System.out.println("maior de idade");
+    }else{
+        System.out.println("menor de idade");
+    }
+}
